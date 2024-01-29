075c3d6872f3f4e52e4a4709d203f248b73b336c
William Barnard <barnardwill@gmail.com>
2024-01-28 18:23:58 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
   }
 })
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
